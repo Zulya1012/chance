59a4988c52c95a5665201ca6792c2ca9d6ce2e7f
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-20 14:45:42 +0400

    Update shans.java

--- shans.java
+++ shans.java
@@ -1,51 +1,41 @@
 import java.util.Scanner;
- public class shans{
-   public static void main(String[] arg){
-int y=5;
-Scanner sc=new Scanner(System.in);
-System.out.println("Vvedite chislo:");
-int a=sc.nextInt();
+public class shans {
+  public static void main(String[] arg) {
+      int y = 5;
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Vvedite chislo:");
+      int a = sc.nextInt();
 
-int c=a-y;
+      int c = a - y;
 
-if (c==0){
- System.out.println("True");
-}
+      if (c == 0) {
+        System.out.println("True");
+      } else {
 
-else { 
+        System.out.println("False");
+        //Scanner s=new Scanner(System.in);
+        System.out.println("Vvedite chislo zanavo:");
+        int b = sc.nextInt();
 
-System.out.println("False");
-//Scanner s=new Scanner(System.in);
-System.out.println("Vvedite chislo zanavo:");
-int b=sc.nextInt();
+        int d = b - y;
 
+        if (d == 0) {
+          System.out.println("True");
+        } else {
+          //Scanner scr=new Scanner(System.in);
+          System.out.println("False");
+          System.out.println("Vvedite chislo zanovo:");
+          int k = sc.nextInt();
 
-int d=b-y;
+          int p = k - y;
 
-if (d==0){
- System.out.println("True");
-}
+          if (p == 0) {
+            System.out.println("True");
+          } else {
+            System.out.println("False");
+          }
 
-else {
-//Scanner scr=new Scanner(System.in);
-System.out.println("False");
-System.out.println("Vvedite chislo zanovo:");
-int k=sc.nextInt();
-
-int p=k-y;
-
-if (p==0){
-System.out.println("True");
-}
-
-else{
-System.out.println("False");
-}
-
-
-
-}
-
+        }
  }
 
 }
