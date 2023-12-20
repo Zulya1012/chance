0cc063d27f53e39a341f6a997a22cd503acc78c8
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-20 14:44:14 +0400

    Add files via upload

---
+++ shans.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+ public class shans{
+   public static void main(String[] arg){
+int y=5;
+Scanner sc=new Scanner(System.in);
+System.out.println("Vvedite chislo:");
+int a=sc.nextInt();
+
+int c=a-y;
+
+if (c==0){
+ System.out.println("True");
+}
+
+else { 
+
+System.out.println("False");
+//Scanner s=new Scanner(System.in);
+System.out.println("Vvedite chislo zanavo:");
+int b=sc.nextInt();
+
+
+int d=b-y;
+
+if (d==0){
+ System.out.println("True");
+}
+
+else {
+//Scanner scr=new Scanner(System.in);
+System.out.println("False");
+System.out.println("Vvedite chislo zanovo:");
+int k=sc.nextInt();
+
+int p=k-y;
+
+if (p==0){
+System.out.println("True");
+}
+
+else{
+System.out.println("False");
+}
+
+
+
+}
+
+ }
+
+}
+
+}
